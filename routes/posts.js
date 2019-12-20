e3f646956742c5729da3d04f66524d0926f5024f
Kyla Johnson <johnsonkyla18@yahoo.com>
2019-12-19 18:04:13 -0600

    posts, connected to mongo, sent through postman

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,27 @@
 const express = require('express');
-
 const router = express.Router();
+const Post = require('../models/Post');
 
-router.get('/posts', (req, res) => {
-    res.send('We are on posts');
+router.get('/', async (req, res) => {
+    try{
+
+    }catch(err){
+        res.json({message:err});
+    }
+});
+
+
+router.post('/', async (req,res) => {
+    const post = new Post({
+        title: req.body.title,
+        description: req.body.description
+    });
+    try{
+    const savedPost = await post.save()
+        res.json(savedPost);
+    }catch(err){
+        res.json({ message: err });
+    }
 });
 
 module.exports = router;
